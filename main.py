6c8f592695632280415677c13726420699c20264
maodas <rodas807@gmail.com>
2025-06-02 22:10:41 -0600

    UI basic design

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ from sys import exit
 #Main Class
 class FitTrack(QWidget):
     def __init__(self):
-                super().__init__()
+        super().__init__()
+        self.settings()
+        self.initUI()
+    #settings
+    def settings(self):
+        self.setWindowTitle("FitTrack")
+        self.resize(800,600)
     #init UI
     def initUI(self):
         self.date_box = QDateEdit()
@@ -34,7 +40,57 @@ class FitTrack(QWidget):
         self.delete_btn = QPushButton("Delete")
         self.clear_btn = QPushButton("Clear")
         self.dark_mode = QCheckBox("Dark Mode")
-             
+
+
+        self.table = QTableWidget()
+
+
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        #Design our layout
+        self.master_layout = QHBoxLayout()
+        self.col1 = QVBoxLayout()
+        self.col2 = QVBoxLayout()
+
+        self.sub_row1 = QHBoxLayout()
+        self.sub_row2 = QHBoxLayout()
+        self.sub_row3 = QHBoxLayout()
+        self.sub_row4 = QHBoxLayout()
+
+        self.sub_row1.addWidget(QLabel("Date:"))
+        self.sub_row1.addWidget(self.date_box)
+
+        self.sub_row2.addWidget(QLabel("Calories"))
+        self.sub_row2.addWidget(self.kal_box)
+
+        self.sub_row3.addWidget(QLabel("KM:"))
+        self.sub_row3.addWidget(self.distance_box)
+
+        self.sub_row4.addWidget(QLabel("Description"))
+        self.sub_row4.addWidget(self.description)
+
+        self.col1.addLayout(self.sub_row1)       
+        self.col1.addLayout(self.sub_row2)
+        self.col1.addLayout(self.sub_row3)
+        self.col1.addLayout(self.sub_row4)      
+        self.col1.addWidget(self.dark_mode)
+
+        btn_row1 = QHBoxLayout()
+        btn_row2 = QHBoxLayout()
+
+        btn_row1.addWidget(self.add_btn)
+        btn_row1.addWidget(self.delete_btn)
+        btn_row2.addWidget(self.submit_btn)
+        btn_row2.addWidget(self.clear_btn)
+
+        self.col1.addLayout(btn_row1)
+        self.col1.addLayout(btn_row2)
+        
+        self.master_layout.addLayout(self.col1)
+        self.setLayout(self.master_layout)
+
+                
     #Load Tables
     #Add Tables
     #Delete Tables
@@ -44,3 +100,9 @@ class FitTrack(QWidget):
     #Reset
 
 #Initizaly my DB
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main = FitTrack()
+    main.show()
+    app.exec_()
